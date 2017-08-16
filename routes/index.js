557d0067bf44a78a85afdb9667795587d13a020f
meikangfu <meikangfu@jd.com>
2017-08-16 23:09:24 +0800

    add

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var fs = require('fs');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -8,6 +9,16 @@ router.get('/', function(req, res, next) {
 
 router.get('/app', function (req, res, next) {
   res.render('app');
+});
+
+router.get('/app/help', function (req, res, next) {
+  var base = "images/image.png";
+  var cam_path = "camera/cam.jpg";
+  if (fs.existsSync("../public/"+cam_path)){
+    res.render('help', {cam_path: cam_path});
+  } else {
+    res.render('help', {cam_path: base})
+  }
 });
 
 module.exports = router;
